f3de831758edaa46a3c7d22578f7a77f218aeba6
Jbarbss <jbarbanel@gmail.com>
2021-06-08 19:50:44 -0400

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
