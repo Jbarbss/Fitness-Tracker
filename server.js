fe47e05edd87a1c5c6b40ecef95b5fb41ef10078
Jbarbss <jbarbanel@gmail.com>
2021-06-07 22:30:48 -0400

    fixed issue on api routes

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const logger = require("morgan")
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(logger("dev"))
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
@@ -17,10 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker
   useFindAndModify: false
 });
 
-// routes
-require("./routes/apiRoutes.js");
-require("./routes/htmlRoutes.js");
-
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
